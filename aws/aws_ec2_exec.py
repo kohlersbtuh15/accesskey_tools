4409d58509ec679f5551c18f64794067a0df5f16
kohlersbtuh15 <kohlersbtuh15@gmail.com>
2024-05-08 18:06:45 +0800

    fix bug

--- aws/aws_ec2_exec.py
+++ aws/aws_ec2_exec.py
@@ -5,7 +5,9 @@ import time
 import aws_select_iam
 from enumerate_iam.main import get_client
 import urllib3
+
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
 
 # default_socket = socket.socket
 # socks.set_default_proxy(socks.SOCKS5, config.SOCKS5_PROXY_HOST, config.SOCKS5_PROXY_PORT)
@@ -13,52 +15,41 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 def query_ec2_instances(AccessKeyID, AccessKeySecret):
     ec2_info = {}
-    sec_groups_info = {}
-    ec2 = boto3.client('ec2', region_name='us-east-1', aws_access_key_id=AccessKeyID, aws_secret_access_key=AccessKeySecret)
+    Agent_info = {}
+    ec2 = boto3.client('ec2', region_name='us-east-1', aws_access_key_id=AccessKeyID,
+                       aws_secret_access_key=AccessKeySecret)
     response = ec2.describe_regions()
     for region in response['Regions']:
         RegionId = region['RegionName']
         print("正在检索: " + RegionId)
-        ec2_client = get_client(access_key=AccessKeyID, secret_key=AccessKeySecret, service_name='ec2',session_token=None,
-                        region=RegionId)
-        ssm_client = get_client(access_key=AccessKeyID, secret_key=AccessKeySecret, service_name='ssm', session_token=None,
-                        region=RegionId)
+        ec2_client = get_client(access_key=AccessKeyID, secret_key=AccessKeySecret, service_name='ec2',
+                                session_token=None,
+                                region=RegionId)
+        ssm_client = get_client(access_key=AccessKeyID, secret_key=AccessKeySecret, service_name='ssm',
+                                session_token=None,
+                                region=RegionId)
         try:
-            ssm_ec2_info = ssm_client.describe_instance_information()['InstanceInformationList']
-            print("agent信息：")
-            print(ssm_ec2_info)
+            ssm_ec2_infos = ssm_client.describe_instance_information()['InstanceInformationList']
+            for ssm_ec2_info in ssm_ec2_infos:
+                Agent_InstanceId = ssm_ec2_info['InstanceId']
+                Agent_info[Agent_InstanceId] = ssm_ec2_info
             response = ec2_client.describe_instances()
-            for reservation in response['Reservations']:
-                for instance in reservation['Instances']:
-                    InstanceId = instance['InstanceId']
-                    ec2_info[InstanceId] = {}
-                    ec2_info[InstanceId]['State'] = instance['State']['Name']
-                    ec2_info[InstanceId]['KeyName'] = instance.get('KeyName')
-                    ec2_info[InstanceId]['Tags'] = instance.get('Tags')
-                    ec2_info[InstanceId]['AvailabilityZone'] = instance['Placement']['AvailabilityZone']
+            while True:
+                for reservation in response['Reservations']:
+                    InstanceId = reservation.get('Instances', [])[0].get('InstanceId')
+                    ec2_info[InstanceId] = reservation.get('Instances', [])[0]
                     ec2_info[InstanceId]['RegionId'] = RegionId
-                    ec2_info[InstanceId]['PublicIpAddress'] = instance.get('PublicIpAddress')
-                    ec2_info[InstanceId]['PrivateIpAddress'] = instance.get('PrivateIpAddress')
-                    for Group_info in instance['SecurityGroups']:
-                        GroupId = Group_info['GroupId']
-                        sec_group_response = ec2_client.describe_security_groups(GroupIds=[
-                            GroupId,
-                        ])
-                        ec2_info[InstanceId]['SecurityGroups'] = sec_group_response['SecurityGroups']
-                    ec2_info[InstanceId]['ImageId'] = instance.get('ImageId')
-                    ec2_info[InstanceId]['SubnetId'] = instance.get('SubnetId')
-                    ec2_info[InstanceId]['VpcId'] = instance.get('VpcId')
-                    ec2_info[InstanceId]['IamInstanceProfile'] = instance.get('IamInstanceProfile')
-                    if instance.get('Platform') is not None:
-                        ec2_info[InstanceId]['PlatformDetail'] = instance.get('Platform')
-                    else:
-                        ec2_info[InstanceId]['PlatformDetail'] = instance.get('PlatformDetails')
-                    ec2_info[InstanceId]['Architecture'] = instance.get('Architecture')
-                    ec2_info[InstanceId]['RootDeviceName'] = instance.get('RootDeviceName')
+                    ec2_info[InstanceId]['Agent'] = Agent_info.get(InstanceId)
+                if "nextToken" in response:
+                    response = ec2_client.describe_instances(
+                        nextToken=response['nextToken']
+                    )
+                else:
+                    break
         except AttributeError as e:
             print(e)
-        continue
     return ec2_info
+
 
 def create_instance_profile(iam_client):
     with open("amazon_ssm_managed_instance_core.json",
@@ -92,6 +83,7 @@ def create_instance_profile(iam_client):
         RoleName='AmazonSSMManagedInstance')
     return instance_profile_arn, instance_profile_name
 
+
 def delete_instance_profile(AccessKeyID, AccessKeySecret):
     iam_client = boto3.client('iam', aws_access_key_id=AccessKeyID, aws_secret_access_key=AccessKeySecret)
     response = iam_client.list_users()
@@ -124,6 +116,43 @@ def delete_instance_profile(AccessKeyID, AccessKeySecret):
     except Exception as err:
         print(err)
 
+
+def associate_iam_add(RegionId, AccessKeyID, AccessKeySecret, InstanceId):
+    instance_profile_arn, instance_profile_name = get_instance_profile(AccessKeyID, AccessKeySecret)
+    print(instance_profile_arn)
+    try:
+        client_ec2 = boto3.client('ec2', region_name=RegionId, aws_access_key_id=AccessKeyID,
+                                  aws_secret_access_key=AccessKeySecret)
+        response = client_ec2.associate_iam_instance_profile(
+            IamInstanceProfile={
+                'Arn': instance_profile_arn,
+                'Name': instance_profile_name,
+            },
+            InstanceId=InstanceId)
+        if response.get("ResponseMetadata").get("HTTPStatusCode") == 200:
+            print(
+                "实例配置文件关联成功，但是生效需要一定的等待时间，一般10分钟左右，请稍后再执行命令"
+            )
+        else:
+            print("ec2实例配置文件关联失败")
+    except Exception:
+        print("实例配置文件创建成功,但是关联失败，请重新执行")
+    return True
+
+
+def associate_iam_delete(RegionId, AccessKeyID, AccessKeySecret, InstanceId):
+    client_ec2 = boto3.client('ec2', region_name=RegionId, aws_access_key_id=AccessKeyID,
+                              aws_secret_access_key=AccessKeySecret)
+    responses = client_ec2.describe_iam_instance_profile_associations()
+    for response in responses['IamInstanceProfileAssociations']:
+        if InstanceId == response['InstanceId']:
+            AssociationId = response['AssociationId']
+            response = client_ec2.disassociate_iam_instance_profile(
+                AssociationId=AssociationId,
+            )
+        time.sleep(1)
+
+
 def get_instance_profile(AccessKeyID, AccessKeySecret):
     iam_client = boto3.client('iam', aws_access_key_id=AccessKeyID, aws_secret_access_key=AccessKeySecret)
     response = iam_client.list_instance_profiles(PathPrefix='/', MaxItems=123)
@@ -138,10 +167,17 @@ def get_instance_profile(AccessKeyID, AccessKeySecret):
     instance_profile_arn, name = create_instance_profile(iam_client)
     return instance_profile_arn, name
 
+
 def commad_exec(AccessKeyID, AccessKeySecret, InstanceId, cmd, com_type, RegionId):
     if cmd == '':
         cmd = input("please input cmd:")
-    ssm_client = boto3.client('ssm', region_name=RegionId, aws_access_key_id=AccessKeyID, aws_secret_access_key=AccessKeySecret)
+    ssm_client = boto3.client('ssm', region_name=RegionId, aws_access_key_id=AccessKeyID,
+                              aws_secret_access_key=AccessKeySecret)
+    print(InstanceId)
+    print(com_type)
+    if com_type is None:
+        com_type = input("please input com_type AWS-RunShellScript or AWS-RunPowerShellScript: ")
+    print(cmd)
     response = ssm_client.send_command(
         InstanceIds=[
             InstanceId,
@@ -169,6 +205,7 @@ def commad_exec(AccessKeyID, AccessKeySecret, InstanceId, cmd, com_type, RegionI
         "StandardErrorContent").strip()
     print(cmd_output)
 
+
 if __name__ == '__main__':
     AccessKeyID = config.AccessKeyID
     AccessKeySecret = config.AccessKeySecret
@@ -181,9 +218,8 @@ if __name__ == '__main__':
     if not ec2_info:
         print("no result")
         exit(0)
-    is_Associates2 = input("是否删除已经配置的角色和策略 T/F：")
-    if is_Associates2 == "T":
-        delete_instance_profile(AccessKeyID, AccessKeySecret)
+
+    # AWS-RunShellScript code
     platform_dic = {
         "Linux": "AWS-RunShellScript",
         "windows": "AWS-RunPowerShellScript",
@@ -191,68 +227,30 @@ if __name__ == '__main__':
     com_type = None
     InstanceId = input("请输入选择的instanceId:")
     RegionId = ec2_info[InstanceId]['RegionId']
-    if "Linux" in ec2_info[InstanceId]['PlatformDetail']:
-        com_type = platform_dic.get('Linux')
-    elif "windows" in ec2_info[InstanceId]['PlatformDetail']:
-        com_type = platform_dic.get('windows')
-    else:
-        com_type = input("无法判断机器平台，请手动输入'Linux' 或 'windows': ")
-    is_Associates = None
     while True:
-        # 未绑定自动关联，已绑定的话手动选择是否删除配置T/F
-        if not ec2_info[InstanceId]['IamInstanceProfile'] and is_Associates == "F":
-            print("检测到当前ec2主机未关联实例配置文件，正在尝试关联...")
-            instance_profile_arn, instance_profile_name = get_instance_profile(AccessKeyID, AccessKeySecret)
-            print(instance_profile_arn)
-            try:
-                client_ec2 = boto3.client('ec2', region_name=RegionId, aws_access_key_id=AccessKeyID, aws_secret_access_key=AccessKeySecret)
-                response = client_ec2.associate_iam_instance_profile(
-                    IamInstanceProfile={
-                        'Arn': instance_profile_arn,
-                        'Name': instance_profile_name,
-                    },
-                    InstanceId=InstanceId)
-                if response.get("ResponseMetadata").get("HTTPStatusCode") == 200:
-                    ec2_info[InstanceId]['IamInstanceProfile'] = instance_profile_arn
-                    print(
-                        "实例配置文件关联成功，但是生效需要一定的等待时间，一般10分钟左右，请稍后再执行命令"
-                    )
-                    continue
-                else:
-                    print("ec2实例配置文件关联失败")
-            except Exception:
-                print("实例配置文件创建成功,但是关联失败，请重新执行")
+        if "Linux" in ec2_info[InstanceId]['PlatformDetails']:
+            com_type = platform_dic.get('Linux')
+        elif "windows" in ec2_info[InstanceId]['PlatformDetails']:
+            com_type = platform_dic.get('windows')
         else:
-            is_Associates = input("是否取消当前ec2实例【 {} 】与策略关联 T/F/q：".format(InstanceId))
-            if is_Associates == "T":
-                client_ec2 = boto3.client('ec2', region_name=RegionId, aws_access_key_id=AccessKeyID,
-                                          aws_secret_access_key=AccessKeySecret)
-                responses = client_ec2.describe_iam_instance_profile_associations()
-                for response in responses['IamInstanceProfileAssociations']:
-                    if InstanceId == response['InstanceId']:
-                        AssociationId = response['AssociationId']
-                        response = client_ec2.disassociate_iam_instance_profile(
-                            AssociationId=AssociationId,
-                        )
-                    time.sleep(1)
-                    ec2_info = query_ec2_instances(AccessKeyID, AccessKeySecret)
-                print("已取消权限绑定，请自行查看配置，如下：")
-                print(ec2_info)
-            elif is_Associates == "q":
-                break
-            if ec2_info[InstanceId]['IamInstanceProfile'] is not None:
-                cmd = ''
-                try:
-                    commad_exec(AccessKeyID, AccessKeySecret, InstanceId, cmd, com_type, RegionId)
-                except Exception as err:
-                    print("策略绑定可能未生效，请等待一会儿(大概10分钟)再执行该脚本。具体看SSM agent是否绑定。")
-                    print(err)
-                    continue
-                flag = input("输入q退出，其他字符继续:")
-                if flag == 'q':
-                    break
-                is_continue = input("重新选择InstanceId请输入yes:")
-                if is_continue == 'yes':
-                    print(ec2_info)
-                    com_type = None
-                    InstanceId = input("请输入选择的instanceId:")
+            com_type = input("无法判断机器平台，请手动输入'AWS-RunShellScript' 或 'AWS-RunPowerShellScript': ")
+
+        if not ec2_info[InstanceId].get('IamInstanceProfile'):
+            if associate_iam_add(RegionId, AccessKeyID, AccessKeySecret, InstanceId):
+                time.sleep(2)
+        cmd = ''
+        try:
+            commad_exec(AccessKeyID, AccessKeySecret, InstanceId, cmd, com_type, RegionId)
+            if not ec2_info[InstanceId].get('IamInstanceProfile'):
+                associate_iam_delete(RegionId, AccessKeyID, AccessKeySecret, InstanceId)
+                delete_instance_profile(AccessKeyID, AccessKeySecret)
+        except Exception as err:
+            print("策略绑定可能未生效，请等待一会儿(大概10分钟)再执行该脚本。具体看SSM agent是否绑定。")
+            print(err)
+            continue
+        is_continue = input("重新选择InstanceId请输入yes，退出请输入q，任意输入继续执行其他命令:")
+        if is_continue == 'q':
+            break
+        elif is_continue == 'yes':
+            com_type = None
+            InstanceId = input("请输入选择的instanceId:")
